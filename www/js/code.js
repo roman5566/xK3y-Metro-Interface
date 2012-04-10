8bdb9a6375285d627864f6fda7ef8c9449cc58e5
Mr_Waffle <mrwafflewaffle@aim.com>
2012-04-10 22:53:03 +0200

    Overall cleanup, more use of native functions, defined all Favorite functions

--- www/js/code.js
+++ www/js/code.js
@@ -1,4 +1,4 @@
-var version='0.03';
+var version='0.04';
 //Some global variables needed
 var data;
 var saveData;
@@ -113,7 +113,7 @@ function makeCoverWallPage() {
 			iso = ISOlist[i].name;
 			id = ISOlist[i].id;
 			cover = ISOlist[i].image;
-			HTML+='<a href="#details-page?'+id+'&'+escape(iso)+'"><div class="tile accent animate" style="background-image:url(\''+cover+'\'); background-size: 173px;"><span class="tile-title tile-animate">'+iso+'</span></div></a>';
+			HTML+='<a href="#details-page?'+id+'&'+escape(iso)+'"><div class="tile accent animate" style="background-image:url(\''+cover+'\'); background-size: 173px;"><span class="tile-title">'+iso+'</span></div></a>';
 			cur++;
 			/*if (cur == 2) {
 				HTML+='<br/>';
@@ -150,7 +150,7 @@ function makeListPage(args) {
 		var iso, id, cover, letter, activeClass;
 		var lastLetter='';
 		var active = data.active;
-		var HTML='<div class="page-wrapper"><div class="spacer"></div><div class="spacer"></div><span class="page-title">list</span></div>';
+		var HTML='';
 		var l=ISOlist.length;
 		for (var i=0;i<l;i++) {
 			iso = ISOlist[i].name;
@@ -170,9 +170,9 @@ function makeListPage(args) {
 			}
 			HTML+='<a href="#details-page?'+id+'&'+escape(iso)+'"><div class="list-item game '+id+'"><div class="list-item-icon accent" style="background-image:url(\''+cover+'\'); background-size: 72px;"></div><span class="list-item-text'+activeClass+'">'+iso+'</span></div></a>';
 		}
-		HTML+='</div><br/>';
+		HTML+='<br/>';
 		//Native approach should be faster
-		document.getElementById('list-page').innerHTML=HTML;
+		document.getElementById('listcontainer').innerHTML=HTML;
 		$(".easydate").easydate();
 		listsMade=true;
 		//Trigger accentChange to make sure the list gets the correct colors
@@ -192,7 +192,7 @@ function makeFolderStructurePage(args) {
 		return;
 	}
 	if (!foldersMade) {
-		var dir, dirID, par, par1, chk, id, name, cover, activeClass;
+		var dir, dirID, par, par1, chk, id, name, cover, activeClass, HTML;
 		var active=data.active;
 		//Create directories first
 		var l = data.dirs.length;
@@ -203,7 +203,8 @@ function makeFolderStructurePage(args) {
 			//What if the parent directory is a HDD? Make it the content block
 			if ($('div#'+dir+'-dir').length==0) {
 				//Create a new page
-				$('<div id="'+dir+'-dir" class="page"></div>').html('<div class="spacer"></div><div class="spacer"></div><span class="page-title">'+unescape(dir)+'</span><br/><br/>').appendTo(document.getElementById('main'));
+				HTML='<div id="'+dir+'-dir" class="page"><div class="spacer"></div><div class="spacer"></div><span class="page-title">'+unescape(dir)+'</span><br/><br/></div>';
+				document.getElementById('main').innerHTML+=HTML;
 				//Register new page with empty function
 				pages[dir+'-dir']=function(){};
 			}
@@ -215,7 +216,8 @@ function makeFolderStructurePage(args) {
 				par1 = escape(par1);
 			}
 			if (!document.getElementById(dir)) {
-				$('<a href="#folderstructure-page?'+dir+'-dir">').html('<div class="tile accent" style="background-image:url(\'img/folder.png\'); background-size: 173px;"><span class="tile-title">'+unescape(dir)+'</span></div>').appendTo(document.getElementById(par1));
+				HTML='<a href="#folderstructure-page?'+dir+'-dir"><div class="tile accent" style="background-image:url(\'img/folder.png\'); background-size: 173px;"><span class="tile-title">'+unescape(dir)+'</span></div></a>';
+				document.getElementById(par1).innerHTML+=HTML;
 			}
 		}
 		//Then the ISOs
@@ -238,7 +240,8 @@ function makeFolderStructurePage(args) {
 			if (id==active) {
 				activeClass=' class="activeGame"';
 			}
-			$('<a href="#details-page?'+id+'&'+escape(name)+'">').html('<div class="tile accent animate" style="background-image:url(\''+cover+'\'); background-size: 173px;"><span class="tile-title tile-animate">'+name+'</span></div>').appendTo(document.getElementById(par1));
+			HTML='<a href="#details-page?'+id+'&'+escape(name)+'"><div class="tile accent animate" style="background-image:url(\''+cover+'\'); background-size: 173px;"><span class="tile-title">'+name+'</span></div></a>';
+			document.getElementById(par1).innerHTML+=HTML;
 		};
 		foldersMade=true;
 		accentChange(saveData['Settings'].accent);
@@ -286,23 +289,39 @@ function prepDetails(id, name) {
 	}
 	var url = 'covers/'+id+'.xml';
 	document.getElementById('details-page').innerHTML='';
+	var title, summary, HTML;
 	$.ajax({
 		type: "GET",
 		url: url,
 		dataType: "xml",
 		cache: false,
 		success: function(xml) {
-			//Prepare title HTML
-			var title;
-			if ($(xml).find('title').text()=="No Title") title = unescape(name);
-			else title = $(xml).find('title').text();
-			var summary = $(xml).find('summary').text();
-			var HTML='<div class="spacer"></div><div class="spacer"></div><span class="page-title">'+title+'</span><br/><div class="page-wrapper"><br/><img class="details-cover" src="covers/'+id+'.jpg"/>'+summary+'<div class="details-button-pane"><a class="button" href="javascript:launchGame(\''+id+'\');" style="float:left">Play</a><a class="button" href="javascript:history.back();" style="float:right">Close</a></div></div>';
+			if ($(xml).find('title').text()=="No Title") {
+				title = unescape(name);
+			}
+			else {
+				title = $(xml).find('title').text();
+			}
+			summary = $(xml).find('summary').text();
+			HTML='<div class="spacer"></div><div class="spacer"></div><span class="page-title">'+title+'</span><br/><br/><div class="page-wrapper">';
+			HTML+='<img class="details-cover" src="covers/'+id+'.jpg"/>'+summary+'<div class="details-button-pane">';
+			HTML+='<a class="button" href="javascript:launchGame(\''+id+'\');">Play</a>';
+			HTML+='<a class="button" href="javascript:history.back();">Close</a>';
+			HTML+='<a class="button" href="javascript:pinToMain(\''+id+'\', \''+name+'\');">Pin to start</a>';
+			HTML+='<a class="button" href="javascript:history.back();">Add to favorites</a>';
+			HTML+='</div></div>';
 			document.getElementById('details-page').innerHTML=HTML;
 		},
 		error: function() {
-			var summary="Betrayed by the ruling families of Italy, a young man embarks upon an epic quest for vengeance. To eradicate corruption and restore his family's honor, he will study the secrets of an ancient Codex, written by Altaïr. To his allies, he will become a force for change - fighting for freedom and justice. To his enemies, he will become a dark knight - dedicated to the destruction of the tyrants abusing the people of Italy. His name is Ezio Auditore da Firenze. He is an Assassin."
-			var HTML='<div class="spacer"></div><div class="spacer"></div><span class="page-title">'+name+'</span><br/><div class="page-wrapper"><br/><img class="details-cover" src="img/test.jpg"/>'+summary+'<div class="details-button-pane"><a class="button" href="javascript:launchGame(\''+id+'\');" style="float:left">Play</a><a class="button" href="javascript:history.back();" style="float:right">Close</a></div></div>';
+			title=unescape(name);
+			summary="Betrayed by the ruling families of Italy, a young man embarks upon an epic quest for vengeance. To eradicate corruption and restore his family's honor, he will study the secrets of an ancient Codex, written by Altaïr. To his allies, he will become a force for change - fighting for freedom and justice. To his enemies, he will become a dark knight - dedicated to the destruction of the tyrants abusing the people of Italy. His name is Ezio Auditore da Firenze. He is an Assassin."
+			HTML='<div class="spacer"></div><div class="spacer"></div><span class="page-title">'+title+'</span><br/><br/><div class="page-wrapper">';
+			HTML+='<img class="details-cover" src="img/test.jpg"/>'+summary+'<div class="details-button-pane">';
+			HTML+='<a class="button" href="javascript:launchGame(\''+id+'\');">Play</a>';
+			HTML+='<a class="button" href="javascript:history.back();">Close</a>';
+			HTML+='<a class="button" href="javascript:pinToMain(\''+id+'\', \''+name+'\');">Pin to start</a>';
+			HTML+='<a class="button" href="javascript:history.back();">Add to favorites</a>';
+			HTML+='</div></div>';
 			document.getElementById('details-page').innerHTML=HTML;
 		}
 	});
